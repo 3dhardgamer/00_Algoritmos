15fc1b11615fe0b1f15cd3167db359c72421a221
3dhardgamer <artur.turbas@gmail.com>
2018-05-03 08:53:16 -0300

    Stack.c is the simple implementation of a Stack.

--- Listas/Prep_P1/ex7/Stack.c
+++ Listas/Prep_P1/ex7/Stack.c
@@ -11,14 +11,12 @@ typedef struct element Elem;
 struct stack {
   int count;
   Elem *top;
-  Elem *min;
 };
 
 Stack* create_stack() {
   Stack* stk = (Stack*) malloc(sizeof(Stack));
   if (stk != NULL) {
     stk->top = NULL;
-    stk_min = NULL;
     stk->count = 0;
   }
   return stk;
@@ -51,24 +49,11 @@ int push(Stack* stk, int value) {
   if (stk == NULL) return 0;
 
   Elem *no = (Elem*) malloc(sizeof(Elem));
-  Elem *aux = (Elem*) malloc(sizeof(Elem));
-  if (no == NULL && aux == NULL) return 0;
+  if (no == NULL) return 0;
 
   no->data = value;
   no->prev = stk->top;
   stk->top = no;
-
-  if (stk->min != NULL) {
-    if (value < stk->min->data)
-      aux->data = value;
-    else
-      aux->data = stk->min->data;
-  }
-  else
-    aux->data = value;
-
-  aux->prev = stk->min;
-  stk->min = aux;
   stk->count++;
   return 1;
 }
@@ -76,12 +61,9 @@ int push(Stack* stk, int value) {
 int pop(Stack* stk, int *value) {
   if (stk == NULL || empty_stack(stk)) return 0;
   Elem* no = stk->top; // Aponta para o top do stack
-  Elem* aux = stk->min; // Aponta para o min do stack
   stk->top = no->prev; // Atualiza top do stack para seu antecessor
-  stk->min = aux->prev; // Atualiza min para seu antecessor
   *value = no->data; // Passa como ponteiro o valor do top que foi desempilhado
   free(no); // Libera o top antigo
-  free(aux); // Libera o min antigo
   stk->count--;
   return 1;
 }
