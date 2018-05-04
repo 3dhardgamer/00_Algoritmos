72623c105e01720231d49b716d639ec240b31ee2
3dhardgamer <artur.turbas@gmail.com>
2018-05-03 21:23:03 -0300

    Defined stack operations

--- Listas/Prep_P1/ex3/main.c
+++ Listas/Prep_P1/ex3/main.c
@@ -1,4 +1,109 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+struct element {
+  int data;
+  struct element *prev;
+};
+
+typedef struct element Elem;
+
+struct stack {
+  int count;
+  struct element *topo;
+};
+
+typedef struct stack Stack;
+
+// Definition of STACK operations
+Stack* create_stack();
+void delete_stack();
+int stack_size();
+int empty_stack(Stack* stk);
+void push(Stack* stk, int value);
+void pop(Stack* stk, int *value);
+void top(Stack* stk, int *value);
+
+// Definition of QUEUE operations
+void create_queue();
+void delete_queue();
+void enqueue(int value);
+void dequeue(int *value);
+void front(int *value);
+
+// main flow
+int main () {
+
+  return 1;
+}
+
+
+// STACK Operations...
+Stack* create_stack() {
+  Stack* stk = (Stack*) malloc(sizeof(Stack));
+  if (stk != NULL) {
+    stk->topo = NULL;
+    stk->count = 0;
+  }
+  return stk;
+}
+
+void delete_stack(Stack* stk) {
+  if (stk != NULL) {
+    Elem* no;
+    while (stk->topo != NULL) {
+      no = stk->topo;
+      stk->topo = no->prev;
+      free(no);
+    }
+    free(stk);
+  }
+}
+
+int stack_size(Stack* stk) {
+  return stk->count;
+}
+
+int empty_stack(Stack* stk) {
+  if (stk == NULL) return -1;
+  if (stk->topo == NULL) return 1;
+
+  return 0;
+}
+
+void push(Stack* stk, int value) {
+  if (stk == NULL) return;
+
+  Elem* no = (Elem*) malloc(sizeof(Elem));
+  if (no == NULL) return;
+  no->data = value;
+  no->prev = stk->topo;
+  stk->topo = no;
+  return;
+}
+
+void pop(Stack* stk, int *value) {
+  if (stk == NULL || empty_stack(stk)) return;
+
+  *value = stk->topo->data;
+  Elem* no = stk->topo;
+  stk->topo = no->prev;
+
+  free(no);
+  stk->count--;
+  return;
+}
+
+void top(Stack* stk, int *value) {
+  if (stk == NULL || empty_stack(stk)) return;
+  *value = stk->topo->data;
+  return;
+}
+
+
+
+// QUEUE Operations...
+
+
+
 
