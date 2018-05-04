eb39f47e8f3e5f68d817a86d94518522026653ca
3dhardgamer <artur.turbas@gmail.com>
2018-05-04 18:58:31 -0300

    create stack min files

---
+++ Listas/Prep_P1/ex7/stackmin.c
@@ -0,0 +1,38 @@
+#include <stdlib.h>
+#include "stack.h"
+#include "stackmin.h"
+
+stackmin* stackmin_create() {
+  stackmin* stkmin = malloc(sizeof(stackmin));
+
+  stkmin->def = stack_create();
+  stkmin->aux = stack_create();
+
+  return stkmin;
+}
+
+void stackmin_delete(stackmin* stkmin) {
+  stack_delete(stkmin->def);
+  stack_delete(stkmin->aux);
+  free(stkmin);
+}
+
+void stackmin_push(stackmin* stkmin, int value) {
+  stack_push(stkmin->def, value);
+  if (value < stkmin->aux->top->data) 
+    stack_push(stkmin->aux, value);
+}
+
+int stackmin_pop(stackmin* stkmin) {
+  int value;
+  value = stack_pop(stkmin->def);
+  if (value == stkmin->aux->top->data)
+    stack_pop(stkmin->aux);
+
+  return value;
+}
+
+int stackmin_top(stackmin* stkmin) {
+  return stack_top(stkmin->def);
+}
+
---
+++ Listas/Prep_P1/ex7/stackmin.h
@@ -0,0 +1,17 @@
+#ifndef _STACKMIN_H_
+#define _STACKMIN_H_
+
+typedef struct stackmin stackmin;
+
+struct stackmin {
+  stack *def;
+  stack *aux;
+};
+
+stackmin* stackmin_create();
+void stackmin_delete(stackmin* stkmin);
+void stackmin_push(stackmin* stkmin, int value);
+int stackmin_pop(stackmin* stkmin);
+int stackmin_top(stackmin* stkmin);
+
+#endif
