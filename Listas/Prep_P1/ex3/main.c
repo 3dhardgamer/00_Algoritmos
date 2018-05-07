ab8877d604902f8cb12e4315157b717aca464dab
3dhardgamer <artur.turbas@gmail.com>
2018-05-07 12:00:27 -0300

    Test of queue peek done, Ok!

--- Listas/Prep_P1/ex3/main.c
+++ Listas/Prep_P1/ex3/main.c
@@ -8,6 +8,12 @@ int main() {
   queue_enqueue(qe, 2);
   queue_enqueue(qe, 3);
   printf("%d %d %d\n", queue_dequeue(qe), queue_dequeue(qe), queue_dequeue(qe));
+
+  queue_enqueue(qe, 4);
+  queue_enqueue(qe, 5);
+  queue_enqueue(qe, 6);
+  printf("%d\n", queue_peek(qe));
+
   queue_delete(qe);
 
   return 0;
