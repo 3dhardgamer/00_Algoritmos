1b71c2839ea291864b103d2161895cec7341977c
3dhardgamer <artur.turbas@gmail.com>
2018-05-08 23:40:04 -0300

    create and entry file with the parentheses expression

--- Listas/Prep_P1/ex11/main.c
+++ Listas/Prep_P1/ex11/main.c
@@ -3,12 +3,9 @@
 
 int main() {
   char linebuffer[4096];
-//  char exp[] = "(p(p)p(p)p(p(p)p)p)p";
-//  char exp[] = "(1(2)1)2";
-//  char exp[] = "(1(2)2)1";
-  while (fgets(linebuffer, sizeof(linebuffer), stdin))
-    printf("%s\n", linebuffer);
-
+  fgets(linebuffer, sizeof(linebuffer), stdin);
+  printf("%s\n", linebuffer);
+    
 
   return 0;
 }
